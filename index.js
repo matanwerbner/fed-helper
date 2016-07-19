ca8efd5f7f2ec7b617e1d9f78fe7fead93b3c400
verbmat@gmail.com <1qaz2wsx>
2016-07-19 13:25:36 +0300

    asdfsadf

--- index.js
+++ index.js
@@ -9,13 +9,30 @@ app.set('port', (process.env.PORT || 5000));
 // app.set('views', __dirname + '/views');
 // app.set('view engine', 'ejs');
 
+var urls = [
+	{ url: 'matan.com', json: { value: 'matan' }},
+	{ url: 'yosi.com', json: { value: 'yosi' }}
+]
+
 app.get('/', function(req, res) {
 	res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
+    console.log('looking for ' + req.query.url);
+    var jsonResult = urls.find((u) => u.url == req.query.url);
 
     res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({ url: "myQueryString" }));
+    res.send(JSON.stringify(jsonResult));
+});
+
+app.get('/urls', function(req, res) {
+	res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    var response = urls.map((u) => u.url);
+
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({ urls: response }));
 });
 
 app.listen(app.get('port'), function() {
