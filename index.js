e95370f57cdcfc3bc6762e7e65cac6b71a9b2273
verbmat@gmail.com <1qaz2wsx>
2016-07-19 13:03:28 +0300

    asdfsda

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var url = require('url');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -10,10 +11,12 @@ app.set('port', (process.env.PORT || 5000));
 // app.set('view engine', 'ejs');
 
 app.get('/', function(request, res) {
-  res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({ a: 1 }));
+	var url_parts = url.parse(request.url, true);
+	var query = url_parts.query;
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({ url: query }));
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+    console.log('Node app is running on port', app.get('port'));
 });
